36fe5e01926191eb8ce78ff892db4ce0497c3a86
Adam Jowett <akuma.me@gmail.com>
2016-09-22 07:06:27 +0000

    Accounted for edge case in Safari, updated code documentation

--- logger.service.js
+++ logger.service.js
@@ -24,6 +24,12 @@
         }());
     }());
 
+    /**
+     * @ngdoc service
+     * @module angular-logger-max
+     * @name angular-logger-max.service:Logger
+     * @description Service to provide smarter and more descriptive logs to the console and remote logging services
+     */
     function Logger() {
         var _debug = false,
             _api = {
@@ -45,6 +51,17 @@
 
         return _api;
 
+        /**
+         * @ngdoc method
+         * @name log
+         * @description Method to log out a message or object along the lines of console.log
+         * @methodOf angular-logger-max.service:Logger
+         * @param  {*} prepend   A string or object to prepend the message with
+         * @param  {*} msg       The message to display
+         * @param  {Boolean} fullStack Whether to show the full stack with the message
+         * @param  {Boolean} expand    Whether to expand the structure of any objects or arrays logged out
+         * @return {*}                 The string or object to log to the console
+         */
         function log(prepend, msg, fullStack, expand) {
             if (!_debug) return;
 
@@ -55,6 +72,17 @@
             }
         }
 
+        /**
+         * @ngdoc method
+         * @name info
+         * @description Method to log out a message or object along the lines of console.info
+         * @methodOf angular-logger-max.service:Logger
+         * @param  {*} prepend   A string or object to prepend the message with
+         * @param  {*} msg       The message to display
+         * @param  {Boolean} fullStack Whether to show the full stack with the message
+         * @param  {Boolean} expand    Whether to expand the structure of any objects or arrays logged out
+         * @return {*}                 The string or object to log to the console
+         */
         function info(prepend, msg, fullStack, expand) {
             if (!_debug) return;
 
@@ -65,6 +93,17 @@
             }
         }
 
+        /**
+         * @ngdoc method
+         * @name warn
+         * @description Method to log out a message or object along the lines of console.warn
+         * @methodOf angular-logger-max.service:Logger
+         * @param  {*} prepend   A string or object to prepend the message with
+         * @param  {*} msg       The message to display
+         * @param  {Boolean} fullStack Whether to show the full stack with the message
+         * @param  {Boolean} expand    Whether to expand the structure of any objects or arrays logged out
+         * @return {*}                 The string or object to log to the console
+         */
         function warn(prepend, msg, fullStack, expand) {
             // always show warnings, debug or not
             _formatOutput('warn', 'color: orange', prepend, msg, _formatStackTrace(fullStack), expand);
@@ -74,7 +113,20 @@
             }
         }
 
+        /**
+         * @ngdoc method
+         * @name error
+         * @description Method to log out a message or object along the lines of console.error
+         * @methodOf angular-logger-max.service:Logger
+         * @param  {*} prepend   A string or object to prepend the message with
+         * @param  {*} msg       The message to display
+         * @param  {Boolean} fullStack Whether to show the full stack with the message (defaults to true)
+         * @param  {Boolean} expand    Whether to expand the structure of any objects or arrays logged out
+         * @return {*}                 The string or object to log to the console
+         */
         function error(prepend, msg, fullStack, expand) {
+            fullStack = typeof fullStack === 'undefined' ? true : fullStack;
+
             // always show errors, debug or not
             _formatOutput('error', 'background-color: maroon; font-weight: bold; color: white', prepend, msg, _formatStackTrace(fullStack), expand);
 
@@ -84,6 +136,17 @@
             }
         }
 
+        /**
+         * @ngdoc method
+         * @name data
+         * @description Method to log out the structure of an object or array
+         * @methodOf angular-logger-max.service:Logger
+         * @param  {*} prepend   A string or object to prepend the message with
+         * @param  {*} msg       The message to display
+         * @param  {Boolean} fullStack Whether to show the full stack with the message
+         * @param  {Boolean} expand    Whether to expand the structure of any objects or arrays logged out (defaults to true)
+         * @return {*}                 The string or object to log to the console
+         */
         function data(prepend, msg, fullStack, expand) {
             if (!_debug) return;
 
@@ -97,6 +160,17 @@
             }
         }
 
+        /**
+         * @ngdoc method
+         * @name shout
+         * @description Method to log out a message in a way to make the message more noticable in the console
+         * @methodOf angular-logger-max.service:Logger
+         * @param  {*} prepend   A string or object to prepend the message with
+         * @param  {*} msg       The message to display
+         * @param  {Boolean} fullStack Whether to show the full stack with the message
+         * @param  {Boolean} expand    Whether to expand the structure of any objects or arrays logged out
+         * @return {*}                 The string or object to log to the console
+         */
         function shout(prepend, msg, fullStack, expand) {
             if (!_debug) return;
 
@@ -107,6 +181,17 @@
             }
         }
 
+        /**
+         * @ngdoc method
+         * @name loaded
+         * @description Method to log out a message in a different colour than a standard log
+         * @methodOf angular-logger-max.service:Logger
+         * @param  {*} prepend   A string or object to prepend the message with
+         * @param  {*} msg       The message to display
+         * @param  {Boolean} fullStack Whether to show the full stack with the message
+         * @param  {Boolean} expand    Whether to expand the structure of any objects or arrays logged out
+         * @return {*}                 The string or object to log to the console
+         */
         function loaded(prepend, msg, fullStack, expand) {
             if (!_debug) return;
 
@@ -117,6 +202,17 @@
             }
         }
 
+        /**
+         * @ngdoc method
+         * @name track
+         * @description Method to log out a message in a muted colour than a standard log
+         * @methodOf angular-logger-max.service:Logger
+         * @param  {*} prepend   A string or object to prepend the message with
+         * @param  {*} msg       The message to display
+         * @param  {Boolean} fullStack Whether to show the full stack with the message
+         * @param  {Boolean} expand    Whether to expand the structure of any objects or arrays logged out
+         * @return {*}                 The string or object to log to the console
+         */
         function track(prepend, msg, fullStack, expand) {
             if (!_debug) return;
 
@@ -128,7 +224,7 @@
         }
 
         /**
-         * Function to format the console outputs according to what types of things are passed
+         * @description Function to format the console outputs according to what types of things are passed
          * @param {String}  type        A string to indicate the type of log
          * @param {String}  styles      A string of css styles
          * @param {String}  prepend     Text to prepend the output with
@@ -149,48 +245,59 @@
             if (msg === undefined && typeof prepend !== 'object' && typeof prepend !== 'function') { // if a plain string
                 if (stackString && stackString.length > 0) {
                     console.log(type, styles, prepend, fullStack);
-                } else {
+                }
+                else {
                     console.log(type, prepend, fullStack);
                 }
-            } else if (typeof prepend === 'object') { // if a single object with no prepending text
+            }
+            else if (typeof prepend === 'object') { // if a single object with no prepending text
                 if (expandObj) {
                     if (stackString.length > 0) {
                         console.log(type + JSON.stringify(prepend, null, '\t'), styles, fullStack);
-                    } else {
+                    }
+                    else {
                         console.log(type + JSON.stringify(prepend, null, '\t'));
                     }
-                } else {
+                }
+                else {
                     if (stackString.length > 0) {
                         console.log(type, styles, prepend, fullStack);
-                    } else {
+                    }
+                    else {
                         console.log(type, prepend, fullStack);
                     }
                 }
-            } else { // if prepend and msg exists
+            }
+            else { // if prepend and msg exists
                 if (typeof msg === 'object' && expandObj) { // if msg is an object and it needs to be automatically expanded
                     if (stackString.length > 0) {
                         console.log(type + prepend, styles, JSON.stringify(msg, null, '\t'), fullStack);
-                    } else {
+                    }
+                    else {
                         console.log(type + prepend, JSON.stringify(msg, null, '\t'), fullStack);
                     }
-                } else { // log it out as per normal
+                }
+                else { // log it out as per normal
                     if (stackString.length > 0) {
                         console.log(type + prepend, styles, msg, fullStack);
-                    } else {
+                    }
+                    else {
                         console.log(type + prepend, msg, fullStack);
                     }
                 }
             }
         }
 
-        // get the stack track from the output of a dummy error message so we can provide meaningful path information
+        /**
+         * @description get the stack track from the output of a dummy error message so we can provide meaningful path information
+         */
         function _trace() {
             var err = new Error();
             return err.stack;
         }
 
         /**
-         * Function to format the full or summary stack trace to the console
+         * @description Function to format the full or summary stack trace to the console
          * @param  {Boolean}    fullStack Indicate whether the full stack should be shown in the console or just the filename
          * @return {String}
          */
@@ -216,7 +323,7 @@
         }
 
         /**
-         * Function to return the 2nd, 3rd or nth instance of a needle in a string
+         * @description Function to return the 2nd, 3rd or nth instance of a needle in a string
          * @usage   var PicPath = "/somedirectory/pics/";
          *        	var AppPath = picpath.substring(0, xIndexOf('/', PicPath, 2) + 1);
          * @param  {Number} instance    the number instance to find
@@ -236,7 +343,8 @@
                     }
                 }
                 return found;
-            } else {
+            }
+            else {
                 return 0;
             }
         }
